403afea88e444a28424d7e7bc72090be14877f02
breno otsuka <breno.otsuka@gmail.com>
2020-10-03 19:32:08 -0300

    Refactory project and basic implementation of get_all_cards and create_cards

---
+++ repository/__init__.py
@@ -0,0 +1,2 @@
+from .activity_repository import activities
+from .card_repository import cards
---
+++ repository/activity_repository.py
@@ -0,0 +1,7 @@
+activities = [
+    
+    { 'title': 'OPME', 'subtitle': 'Finalizar conta', 'sla': 5},
+    { 'title': 'Auditoria', 'subtitle': 'Limpar conta', 'sla': 3},
+    { 'title': 'Berçario', 'subtitle': 'Organizar prontuário', 'sla': 2},
+    { 'title': 'Centro Cirurgico', 'subtitle': 'Organizar prontuário', 'sla': 2}
+]
